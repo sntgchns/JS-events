4dcec8cf0bfe571a17283fd274cd97c97d4c6f76
Santiago Soñora <86539574+sntgchns@users.noreply.github.com>
2024-08-08 21:27:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const addBoxButton = document.querySelector("[data-add-box]")
+const grid = document.querySelector(".grid")
+const boxes = document.querySelectorAll(".box")
+
+addBoxButton.addEventListener("click", () => {
+    const box = document.createElement("div")
+    box.classList.add("box")
+    grid.append(box)
+})
+
+function addGlobalEventListener(type, selector, callback, parent = document) {
+    parent.addEventListener(type, e => {
+        if (e.target.matches(selector)) {
+            callback(e)
+        }
+    })
+}
+
+addGlobalEventListener("click", ".box", e => {e.target.classList.toggle("clicked")}, grid)
